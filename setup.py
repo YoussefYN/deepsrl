226f280c38472fba890b6c49499b6f268716c629
Youssef Youssry <youssef.yossry.2014@gmail.com>
2019-06-10 12:36:22 +0300

    Add installation libraries requirements.

--- setup.py
+++ setup.py
@@ -9,5 +9,11 @@ setup(
     author_email='y.ibrahim@innopolis.university',
     license='unlicense',
     packages=['deepsrl', 'deepsrl.neural_srl', 'deepsrl.neural_srl.shared', 'deepsrl.neural_srl.theano'],
+    install_requires=[
+        'numpy',
+        'theano',
+        'protobuf',
+        'nltk'
+    ],
     zip_safe=False
 )
